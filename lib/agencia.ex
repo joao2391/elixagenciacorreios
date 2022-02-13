94742c0babe6e1afb673fd2e2a23bcd61a617a33
joao2391 <j_paulo_15@hotmail.com>
2022-02-13 09:13:54 -0300

    feat: get_agencias

---
+++ lib/agencia.ex
@@ -0,0 +1,4 @@
+defmodule Elixagenciacorreios.Agencia do
+  defstruct nome: "", endereco: "", situacao: ""
+
+end
--- lib/elixagenciacorreios.ex
+++ lib/elixagenciacorreios.ex
@@ -3,16 +3,67 @@ defmodule Elixagenciacorreios do
   Documentation for `Elixagenciacorreios`.
   """
 
+  alias Elixagenciacorreios.Agencia
+
+  #&nbsp
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixagenciacorreios.hello()
-      :world
-
+  Retorna agencias dos Correios filtrada por UF, Municiopio e Bairro.
   """
-  def hello do
-    :world
+  def get_agencias(uf, municipio, bairro) do
+    case HTTPoison.get("https://mais.correios.com.br/app/carrega/carrega_agencia_localidade.php?cmbEstado=#{uf}&cmbMunicipio=#{municipio}&cmbBairro=#{bairro}",
+                        %{"User-Agent" => "elixagencias/1.0.1"}) do
+      {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
+      html = raw_body
+      {:ok, document} = Floki.parse_document(html)
+
+      lista =
+        document
+        |> Floki.find("tbody")
+        |> Floki.find(".tr-base")
+        |> Floki.find(".agencia_desc")
+
+        lista_enderecos = get_enderecos_agencia(lista)
+        lista_situacoes = get_situacoes_agencia(lista)
+        lista_nomes = get_nomes_agencia(lista)
+
+        tamanho_lista = length(lista_enderecos)
+        IO.puts(tamanho_lista)
+
+        for x <- 0..tamanho_lista - 1 do
+          endereco = lista_enderecos |> Enum.at(x)
+          situacao = lista_situacoes |> Enum.at(x)
+          nome = lista_nomes |> Enum.at(x)
+
+          %Agencia{nome: nome, endereco: endereco, situacao: situacao}
+        end
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
   end
+
+  defp get_enderecos_agencia(list) do
+
+    list
+    |> Enum.map(fn {_chave, _valor1, valor2} -> valor2
+                  |> Floki.find(".endereco-agencia") |> Floki.text() end)
+
+  end
+
+  defp get_situacoes_agencia(list) do
+
+    list
+    |> Enum.map(fn {_chave, _valor1, valor2} -> valor2
+                  |> Floki.find(".situacao-agencia") |> Floki.text() end)
+
+  end
+
+  defp get_nomes_agencia(list) do
+
+    list
+    |> Enum.map(fn {_chave, _valor1, valor2} -> valor2
+                  |> Floki.find(".nome-agencia") |> Floki.text() end)
+
+  end
+
 end
